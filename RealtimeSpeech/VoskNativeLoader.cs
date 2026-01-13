20425ba239dcd70029577b11dce4100eb6668637
Wes Brown <wesbrowndev@gmail.com>
2026-01-12 21:24:48 -0500

    Fixed Windows native loader

--- RealtimeSpeech/VoskNativeLoader.cs
+++ RealtimeSpeech/VoskNativeLoader.cs
@@ -1,23 +1,26 @@
 using Godot;
 using System;
 using System.Runtime.InteropServices;
+using System.IO;
 
 public static class VoskNativeLoader
 {
     public static void Load()
     {
+        string osName = OS.GetName();
+
         string platformFolder =
-            OS.GetName() == "Windows" ? "Windows-x64" :
-            OS.GetName() == "macOS" ? "macOS-universal" :
-            OS.GetName() == "Linux" ? "Linux-x86_64" :
-            OS.GetName() == "Android" ? "Android-arm64" :
-                                        "Unknown";
+            osName == "Windows" ? "Windows-x64" :
+            osName == "macOS" ? "macOS-universal" :
+            osName == "Linux" ? "Linux-x86_64" :
+            osName == "Android" ? "Android-arm64" :
+                                  "Unknown";
 
         string libName =
-            OS.GetName() == "Windows" ? "libvosk.dll" :
-            OS.GetName() == "macOS" ? "libvosk.dylib" :
-            OS.GetName() == "Linux" || OS.GetName() == "Android" ? "libvosk.so" :
-                                                                    "Unknown";
+            osName == "Windows" ? "libvosk.dll" :
+            osName == "macOS" ? "libvosk.dylib" :
+            (osName == "Linux" || osName == "Android") ? "libvosk.so" :
+                                                         "Unknown";
 
         string resPath = $"res://RealtimeSpeech/vosklibs/{platformFolder}/{libName}";
         GD.Print("Vosk native lib path: ", resPath);
@@ -32,10 +35,35 @@ public static class VoskNativeLoader
         byte[] data = fa.GetBuffer((long)fa.GetLength());
         fa.Close();
 
-        string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), libName);
-        System.IO.File.WriteAllBytes(tempPath, data);
+        string tempPath;
+
+        if (osName == "Windows")
+        {
+            // Put the DLL next to the executable / game binary
+            string baseDir = AppContext.BaseDirectory;
+            tempPath = Path.Combine(baseDir, libName);
+        }
+        else
+        {
+            tempPath = Path.Combine(Path.GetTempPath(), libName);
+        }
+
+        File.WriteAllBytes(tempPath, data);
 
         GD.Print("Extracted Vosk native lib to: ", tempPath);
+
+        if (osName == "Windows")
+        {
+            try
+            {
+                NativeLibrary.Load(tempPath);
+                GD.Print("Vosk native lib preloaded on Windows.");
+            }
+            catch (Exception e)
+            {
+                GD.PrintErr("Failed to preload Vosk native lib on Windows: ", e.Message);
+            }
+        }
 
         NativeLibrary.SetDllImportResolver(typeof(Vosk.Model).Assembly, (name, assembly, path) =>
         {
